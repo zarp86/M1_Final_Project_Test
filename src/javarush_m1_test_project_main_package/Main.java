2452b3ddf07fdcd1cc10d348b9591659a3985ebb
Artem Zaytsev <zarp1986@gmail.com>
2022-09-18 17:34:31 +0300

    small changes after switching to acsessing remote to SSH-signing way

--- src/javarush_m1_test_project_main_package/Main.java
+++ src/javarush_m1_test_project_main_package/Main.java
@@ -2,6 +2,6 @@ package javarush_m1_test_project_main_package;
 
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("Hi there! after changing remote url to SSH signing");
     }
 }
