ed44bca34c8909e38dabf87fbae368468d05beb6
Artem Zaytsev <zarp1986@gmail.com>
2022-09-18 17:54:55 +0300

    added small changes after 1st successful push command with SSH

--- src/javarush_m1_test_project_main_package/Main.java
+++ src/javarush_m1_test_project_main_package/Main.java
@@ -3,5 +3,6 @@ package javarush_m1_test_project_main_package;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hi there! after changing remote url to SSH signing");
+        System.out.println("...and something else after 1st successful push command with SSH...");
     }
 }
